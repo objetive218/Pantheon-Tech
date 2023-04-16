9a6676713b04348845150e29feec8dcb991c5722
objetive218 <davidsantiagotc@gmail.com>
2023-04-15 21:54:40 -0500

    carritoContext1

--- src/contexts/CarritoContext.jsx
+++ src/contexts/CarritoContext.jsx
@@ -1,10 +1,14 @@
 import { createContext, useState } from "react";
 
-const CarritoContext = createContext()
-const CarritoProvider = function ({children}){
-  const [carrito, setCarrito] = useState(null)
-  return <CarritoContext.Provider value={{carrito,setCarrito}}>{children}</CarritoContext.Provider>
-}
+const CarritoContext = createContext();
+const CarritoProvider = function ({ children }) {
+  const [carrito, setCarrito] = useState(null);
+  return (
+    <CarritoContext.Provider value={{ carrito, setCarrito }}>
+      {children}
+    </CarritoContext.Provider>
+  );
+};
 
-export {CarritoProvider}
+export { CarritoProvider };
 export default CarritoContext;
