66cb99c6158105b4b12841f3638543cc6c47d349
objetive218 <davidsantiagotc@gmail.com>
2023-04-15 19:53:20 -0500

    CarritoContext

---
+++ src/contexts/CarritoContext.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+
+const CarritoContext = createContext()
+const CarritoProvider = function ({children}){
+  const [carrito, setCarrito] = useState(null)
+  return <CarritoContext.Provider value={{carrito,setCarrito}}>{children}</CarritoContext.Provider>
+}
+
+export {CarritoProvider}
+export default CarritoContext;
