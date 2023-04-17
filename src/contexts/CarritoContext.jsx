ae485b55b4c9dfe21038ac1da5b1515cbf774b9e
objetive218 <davidsantiagotc@gmail.com>
2023-04-17 11:24:43 -0500

    carrito1

--- src/contexts/CarritoContext.jsx
+++ src/contexts/CarritoContext.jsx
@@ -2,9 +2,30 @@ import { createContext, useState } from "react";
 
 const CarritoContext = createContext();
 const CarritoProvider = function ({ children }) {
-  const [carrito, setCarrito] = useState(null);
+  const [carrito, setCarrito] = useState({ items: [], subTotal: 0 });
+  const addModel = function (elemento, cantidad) {
+    setCarrito({
+      items: [
+        ...carrito.items,
+        {
+          id: elemento.id,
+          modelo: elemento.modelo,
+          categoria: elemento.categoria,
+          precio: elemento.precio,
+          cantidad: cantidad,
+        },
+      ],
+      subTotal: carrito.items
+        .map(function (item) {
+          return item.precio * item.cantidad;
+        })
+        .reduce(function (actual, siguiente) {
+          return (actual += siguiente);
+        }, 0),
+    });
+  };
   return (
-    <CarritoContext.Provider value={{ carrito, setCarrito }}>
+    <CarritoContext.Provider value={{ carrito, setCarrito, addModel }}>
       {children}
     </CarritoContext.Provider>
   );
---
+++ src/data/catalogo.js
@@ -0,0 +1,10 @@
+let catalogo = [];
+catalogo.push({
+  id: 0,
+  modelo: "samsung",
+  especificaciones: "algo",
+  precio: 1200000,
+  categoria: "celulares",
+});
+
+export { catalogo };
