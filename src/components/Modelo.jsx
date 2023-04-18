b72a20d6e3ef17d77103dacac54d6d852fa1f068
objetive218 <davidsantiagotc@gmail.com>
2023-04-18 12:19:07 -0500

    modelo2

--- src/components/Modelo.jsx
+++ src/components/Modelo.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import modeloStyle from "../css/Modelo.module.css";
 
 const Modelo = (props) => {
   return (
     <>
-      <img src={props.fuente} alt="imagen modelo" />
-      <h2>{props.modelo}</h2>
-      <p>{props.especificaciones}</p>
-      <p>{props.precio}</p>
-      <button onClick={props.clic}>carrito</button>
-      <button onClick={props.clic1}>eliminar</button>
+      <section className={modeloStyle.modelo}>
+        <img className={modeloStyle.image} src={props.fuente} alt="imagen modelo" />
+        <h2>{props.modelo}</h2>
+        <p>{props.especificaciones}</p>
+        <p>{props.precio}</p>
+        <button onClick={props.clic}>carrito</button>
+        <button onClick={props.clic1}>eliminar</button>
+      </section>
     </>
   );
 };
