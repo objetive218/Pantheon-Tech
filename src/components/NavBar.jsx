7ed1cee8e031d37aec9db1daac0e92e106d65dac
“KarenL23” <lopezkaren23.05@gmail.com>
2023-04-20 15:44:32 -0500

    css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,35 @@ import React from "react";
 import StyleNavBar from "../css/NavBar.module.css";
 import { Link } from "react-router-dom";
 import LogoPantheon from "../img/logo.svg";
+import CelularesTitle from"../img/celulares.svg";
+import ComputadorTitle from"../img/computadores.svg";
+import VideojuegoTitle from"../img/videojuegos.svg";
+import AudioTitle from"../img/audio.svg";
+import {BsFillPersonFill, TiShoppingCart} from 'react-icons/Bs, react-icons/Ti';
+
 
 const NavBar = () => {
   return (
-    <nav id="nav">
+    <nav className={StyleNavBar.header}>
       <Link to="/">
         <img src={LogoPantheon} alt="" />
       </Link>
-      <Link to="/celulares">celulares</Link>
-      <Link to="/computadores">Computadores</Link>
-      <Link to="/consolas">Consolas Y Videojuegos</Link>
-      <Link to="/audio">Audio y Accesorios</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/carrito">carrito</Link>
+      <Link to="/celulares">
+        <img src={CelularesTitle} alt="celulares" />
+      </Link>
+      <Link to="/computadores">
+        <img src={ComputadorTitle} alt="computadores" />
+      </Link>
+      <Link to="/consolas">
+        <img src={VideojuegoTitle} alt="videojuegos" />
+      </Link>
+      <Link to="/audio">
+        <img src={AudioTitle} alt="audio" />
+      </Link>
+      <Link to="/buscar">
+      </Link>
+      <Link className={StyleNavBar.logo} to="/login"><BsFillPersonFill/></Link>
+      <Link className={StyleNavBar.carrito} to="/carrito"><TiShoppingCart/></Link>
     </nav>
   );
 };
