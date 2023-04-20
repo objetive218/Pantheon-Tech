656ba16ab294785078358af656b5504834939b01
objetive218 <davidsantiagotc@gmail.com>
2023-04-20 14:25:04 -0500

    catalogo casi final

--- src/contexts/CatalogoContext.jsx
+++ src/contexts/CatalogoContext.jsx
@@ -1,8 +1,13 @@
-import React,{createContext} from "react";
+import React,{createContext, useState} from "react";
 import { catalogo } from "../data/catalogo";
 
 const CatalogoContext = createContext(catalogo);
 const CatalogoProvider = function({children}){
+  const [productos, setProductos] = useState(catalogo);
 
-  return <CatalogoContext.Provider value={}>{}</CatalogoContext.Provider>
+  
+  return <CatalogoContext.Provider value={{productos, setProductos}}>{children}</CatalogoContext.Provider>
 }
+
+export {CatalogoProvider}
+export default CatalogoContext
