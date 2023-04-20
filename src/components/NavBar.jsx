71ef95f29ca2aaa485f8e15bca1e0973a6b4a46b
objetive218 <davidsantiagotc@gmail.com>
2023-04-20 11:25:06 -0500

    logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import StyleNavBar from "../css/NavBar.css";
+import StyleNavBar from "../css/NavBar.module.css";
 import { Link } from "react-router-dom";
+import LogoPantheon from "../img/logo.svg";
 
 const NavBar = () => {
   return (
     <nav id="nav">
       <Link to="/">
-        <img src="" alt="" />
+        <img src={LogoPantheon} alt="" />
       </Link>
       <Link to="/celulares">celulares</Link>
       <Link to="/computadores">Computadores</Link>
       <Link to="/consolas">Consolas Y Videojuegos</Link>
       <Link to="/audio">Audio y Accesorios</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/carrito">carrito</Link>
     </nav>
   );
 };
