d9c8bbafedc133ace96376f3118c723df74dce79
linasanchez38 <125481093+linasanchez38@users.noreply.github.com>
2023-04-20 22:07:51 +0000

    catalogo especificaciones

--- src/data/catalogo.js
+++ src/data/catalogo.js
@@ -2,43 +2,43 @@ let catalogo = [];
 catalogo.push(
   {
     id: 0,
-    modelo: "samsung",
-    especificaciones: "algo",
-    precio: 1200000,
+    modelo: "Apple iPhone 12 128GB",
+    especificaciones: "Tamaño de la pantalla: 6.1 pulgadas, Cámara posterior: 12MP, Cámara frontal: 12MP, Batería: 2775 mAh",
+    precio: "$3'829.000",
     categoria: "celulares",
-    url: "https://www.fayerwayer.com/resizer/-jwPe4qxnuDsKL_diDGE3wMb2Z8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/BLASEIK4SZCVTIAVUVORVMP6L4.jpg",
+    url: "https://falabella.scene7.com/is/image/FalabellaCO/9713518_1?wid=1500&hei=1500&qlt=70"
   },
   {
     id: 1,
-    modelo: "samsung",
-    especificaciones: "algo",
-    precio: 1200000,
+    modelo: "Motorola Edge 30 Neo 128GB"
+    especificaciones: "Tamaño de la pantalla: 6.3 pulgadas, Cámara posterior: 13MP, Cámara frontal: 32MP, Batería: 4020 mAh",
+    precio: "$1'587.900",
     categoria: "celulares",
-    url: "https://www.fayerwayer.com/resizer/-jwPe4qxnuDsKL_diDGE3wMb2Z8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/BLASEIK4SZCVTIAVUVORVMP6L4.jpg",
+    url: "https://falabella.scene7.com/is/image/FalabellaCO/61899453_1?wid=1500&hei=1500&qlt=70",
   },
   {
     id: 2,
-    modelo: "samsung",
-    especificaciones: "algo",
-    precio: 1200000,
+    modelo: "Samsung Galaxy ZFlip3 256GB",
+    especificaciones: "Tamaño de la pantalla: 6.7 pulgadas, Cámara posterior: 12MP, Cámara frontal: 10MP, Batería: 3300 mAh",
+    precio: "$3'177.900",
     categoria: "celulares",
-    url: "https://www.fayerwayer.com/resizer/-jwPe4qxnuDsKL_diDGE3wMb2Z8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/BLASEIK4SZCVTIAVUVORVMP6L4.jpg",
+    url: "https://falabella.scene7.com/is/image/FalabellaCO/gsc_117504291_1629796_1?wid=1500&hei=1500&qlt=70",
   },
   {
     id: 3,
-    modelo: "samsung",
-    especificaciones: "algo",
-    precio: 1200000,
+    modelo: "Xiaomi 11T Pro 256GB",
+    especificaciones: "Tamaño de la pantalla: 6.67 pulgadas, Cámara posterior: 108MP, Cámara frontal: 16MP, Batería: 5000 mAh",
+    precio: "$2'389.900",
     categoria: "celulares",
-    url: "https://www.fayerwayer.com/resizer/-jwPe4qxnuDsKL_diDGE3wMb2Z8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/BLASEIK4SZCVTIAVUVORVMP6L4.jpg",
+    url: "https://falabella.scene7.com/is/image/FalabellaCO/gsc_118192954_2008208_1?wid=1500&hei=1500&qlt=70"
   },
   {
     id: 4,
-    modelo: "samsung",
-    especificaciones: "algo",
-    precio: 1200000,
+    modelo: "",
+    especificaciones: "",
+    precio: "",
     categoria: "computadores",
-    url: "https://www.fayerwayer.com/resizer/-jwPe4qxnuDsKL_diDGE3wMb2Z8=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/BLASEIK4SZCVTIAVUVORVMP6L4.jpg",
+    url: "",
   }
 );
 
