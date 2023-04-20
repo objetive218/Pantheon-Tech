029ed96f8b23380a733eaf3865ba2b61cbf44a28
“AndresCaicedo99” <andriujarfor@gmail.com>
2023-04-20 03:13:50 -0500

    segundo commit andres

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import NavBar from '../css/NavBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    class NavBar extends React.Component {
+        render() {
+          return (
+            <nav id='nav'>
+              <ul>
+                <li><img src="" alt="" /></li>
+                <li><a href="">Inicio</a></li>
+                <li><a href=".//Celulares.jsx">celulares</a></li>
+                <li><a href=".//Computadores.jsx">Computadores</a></li>
+                <li><a href=".//Consolas.jsx">Consolas Y Videojuegos</a></li>
+                <li><a href=".//Audio.jsx">Audio y Accesorios</a></li>
+              </ul>
+            </nav>
+          );
+        }
+      }
   )
 }
 
