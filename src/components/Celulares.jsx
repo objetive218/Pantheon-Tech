dce6ad99afd3bd3b33f3e12acf51b3297dc9c27a
objetive218 <davidsantiagotc@gmail.com>
2023-04-20 19:52:11 -0500

    intento modelos

--- src/components/Celulares.jsx
+++ src/components/Celulares.jsx
@@ -6,46 +6,32 @@ import CatalogoContext from "../contexts/CatalogoContext";
 const Celulares = () => {
   const { productos, setProductos } = useContext(CatalogoContext);
   const [cel, setCel] = useState([]);
-  useEffect(function(){
-    setCel(productos.filter(function (item) {
-      return item.categoria == "celulares";
-    }))
-  },productos)
+  useEffect(
+    function () {
+      setCel(
+        productos.filter(function (item) {
+          return item.categoria == "celulares";
+        })
+      );
+    },
+    [productos]
+  );
   return (
     <>
-      <Modelo
-        fuente={cel[0]?.url}
-        modelo={cel[0]?.modelo}
-        especificaciones={cel[0]?.especificaciones}
-        precio={cel[0]?.precio}
-        id={cel[0]?.id}
-        elemento={cel[0]}
-      />
-
-      <Modelo
-        fuente={cel[1]?.url}
-        modelo={cel[1]?.modelo}
-        especificaciones={cel[1]?.especificaciones}
-        precio={cel[1]?.precio}
-        id={cel[1]?.id}
-        elemento={cel[1]}
-      />
-      <Modelo
-        fuente={cel[2]?.url}
-        modelo={cel[2]?.modelo}
-        especificaciones={cel[2]?.especificaciones}
-        precio={cel[2]?.precio}
-        id={cel[2]?.id}
-        elemento={cel[2]}
-      />
-      <Modelo
-        fuente={cel[3]?.url}
-        modelo={cel[3]?.modelo}
-        especificaciones={cel[3]?.especificaciones}
-        precio={cel[3]?.precio}
-        id={cel[3]?.id}
-        elemento={cel[3]}
-      />
+      {cel != null
+        ? cel.map(function (element, index) {
+            return (
+              <Modelo
+                fuente={cel[index]?.url}
+                modelo={cel[index]?.modelo}
+                especificaciones={cel[index]?.especificaciones}
+                precio={cel[index]?.precio}
+                id={cel[index]?.id}
+                elemento={cel[index]}
+              />
+            );
+          })
+        : ""}
     </>
   );
 };
