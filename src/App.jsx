7bf06e9e1f7067dc2b7346ccec9b2d71fbfb8664
objetive218 <davidsantiagotc@gmail.com>
2023-04-21 14:19:14 -0500

    busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import CatalogoContext from "./contexts/CatalogoContext";
+import BuscadorContext from "./contexts/BuscadorContext";
+import Modelo from "./components/Modelo";
 
 const App = () => {
+  const { productos } = useContext(CatalogoContext);
+  const { buscar } = useContext(BuscadorContext);
+  const [carga, setCarga] = useState([]);
+  useEffect(
+    function () {
+      setCarga(
+        productos.filter(function (item) {
+          return String(item.modelo)
+            .toLowerCase()
+            .includes(String(buscar).toLowerCase());
+        })
+      );
+    },
+    [buscar]
+  );
+
   return (
     <>
-      <h1>hola</h1>
-      <h1>hola</h1>
       <NavBar />
-      <Link to="/">inicio</Link>
+      {buscar == null || String(buscar).length > 3 ? (
+        carga.map(function (element, index) {
+          return (
+            <Modelo key = {index}
+              fuente={carga[index]?.url}
+              modelo={carga[index]?.modelo}
+              especificaciones={carga[index]?.especificaciones}
+              precio={carga[index]?.precio}
+              id={carga[index]?.id}
+              elemento={carga[index]}
+            />
+          );
+        })
+      ) : (
+        <h1>error</h1>
+      )}
     </>
   );
 };
