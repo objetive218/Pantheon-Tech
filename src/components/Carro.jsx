351225d18946d4e01c1144cfc4e94350ed521144
objetive218 <davidsantiagotc@gmail.com>
2023-04-22 20:42:29 -0500

    carro funciona

--- src/components/Carro.jsx
+++ src/components/Carro.jsx
@@ -1,11 +1,33 @@
 import CarritoContext from "../contexts/CarritoContext"
-import React,{useContext, useState} from 'react'
+import React,{useContext, useState, useEffect} from 'react'
+import Modelo from "./Modelo"
 
 const Carro = () => {
   const {carrito} = useContext(CarritoContext)
-  const [lista, setLista] = useState(carrito)
+  const [lista, setLista] = useState([])
+  useEffect(function(){
+    setLista(carrito.items)
+  },[carrito])
+  
   return (
-    <h1>{lista}</h1>
+    <>
+    
+    {lista != null
+        ? (lista.map(function (element, index) {
+            return (
+              <Modelo key = {index}
+                fuente={lista[index]?.url}
+                modelo={lista[index]?.modelo}
+                especificaciones={lista[index]?.especificaciones}
+                precio={lista[index]?.precio}
+                id={lista[index]?.id}
+                elemento={lista[index]}
+              />
+            );
+          }))
+        : ""}
+    </>
+    
   )
 }
 
