cd34a172610cf10288f6670411572196a3ae31f7
objetive218 <davidsantiagotc@gmail.com>
2023-04-22 22:00:25 -0500

    estilo modelo carro

---
+++ src/components/ModeloCar.jsx
@@ -0,0 +1,35 @@
+import React,{useContext} from 'react'
+import CarritoContext from '../contexts/CarritoContext';
+import ModCarStyle from "../css/ModeloCar.module.css"
+
+const ModeloCar = (props) => {
+  const { eliminarModel, addModel } = useContext(CarritoContext);
+  return (
+    <section className={ModCarStyle.box}>
+      <img className={ModCarStyle.image} src={props.url} alt="imagen" />
+      <h1>{props.modelo}</h1>
+      <h2>{props.precio}</h2>
+      <h2>{props.cantidad}</h2>
+      <form action="#">
+          <button
+            type="button"
+            onClick={function () {
+              addModel(props.elemento, 1);
+            }}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            onClick={function () {
+              eliminarModel(props.id);
+            }}
+          >
+            -
+          </button>
+        </form>
+    </section>
+  )
+}
+
+export default ModeloCar
