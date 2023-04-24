89746fbdfe8fd970d43cb01471e01f28c1806500
objetive218 <davidsantiagotc@gmail.com>
2023-04-23 19:48:57 -0500

    imagenes preview

---
+++ src/components/Preview.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Preview = () => {
+  return (
+    <>
+      <picture>
+        <img src="" alt="telefono" />
+        <img src="" alt="telefono preview" />
+      </picture>
+      <picture>
+        <img src="" alt="computador" />
+        <img src="" alt="computador preview" />
+      </picture>
+      <picture>
+        <img src="" alt="consola" />
+        <img src="" alt="consola preview" />
+      </picture>
+      <picture>
+        <img src="" alt="audio" />
+        <img src="" alt="audio preview" />
+      </picture>
+    </>
+  );
+};
+
+export default Preview;
