468249cb84c1064c681171ca1cfad71e9400b621
“KarenL23” <lopezkaren23.05@gmail.com>
2023-04-24 10:59:15 -0500

    svg

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,23 +1,23 @@
 import React from "react";
+import categoria1 from "../img/categoriacelulares.svg";
+import categoria2 from "../img/categoriacomputador.svg";
+import categoria3 from "../img/categoriaconsola.svg";
+import categoria4 from "../img/categoriaaudio.svg";
 
 const Preview = () => {
   return (
     <>
       <picture>
-        <img src="" alt="telefono" />
-        <img src="" alt="telefono preview" />
+        <img src={categoria1} alt="telefono" />
       </picture>
       <picture>
-        <img src="" alt="computador" />
-        <img src="" alt="computador preview" />
+        <img src={categoria2} alt="computador" />
       </picture>
       <picture>
-        <img src="" alt="consola" />
-        <img src="" alt="consola preview" />
+        <img src={categoria3} alt="consola" />
       </picture>
       <picture>
-        <img src="" alt="audio" />
-        <img src="" alt="audio preview" />
+        <img src={categoria4} alt="audio" />
       </picture>
     </>
   );
