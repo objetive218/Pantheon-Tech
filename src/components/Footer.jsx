e284f3387fd684510f65e8c55443ad0cfd68e6a4
NicolasN21s <125577159+NicolasN21s@users.noreply.github.com>
2023-04-25 16:37:51 +0000

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import Footerstyle from "../css/Footer.module.css"
 import React from 'react'
 
 const Footer = () => {
-    return <section className="container__footer">
-        <section className="box__footer">
-            <section className="logo">
+    return <section className={`${FooterStyle.containerFooter}`}>
+        <section className={`${FooterStyle.boxfooter}`}>
+            <section className={`${FooterStyle.logo}`}>
                 <img src="img/" alt="" />
-                <section className="terms">
+                <section className={`${FooterStyle.terms}`}>
                     <h3>Misión:</h3>
                     <p>Pantheon Tech es una empresa dedicada a la distribución, comercialización, y
                         venta de equipos y partes de computo, portátiles, videojuegos, consolas, y
@@ -17,9 +17,11 @@ const Footer = () => {
                     <p>Seremos la principal empresa en importación y distribución de tecnología, ampliaremos nuestra infraestructura
                      a nivel nacional para ofrecer una mejor cobertura y mejorar el servicio al cliente.
                     </p>
-                    <section className="box__footer">
+                    <section className={`${FooterStyle.boxfooter}`}>
                         <h2>Términos y Condiciones</h2>
-                        <p></p>
+                        {!isClicked && (
+                            <button onClick={}
+                        )}
                     </section>
                     <h2>Redes Sociales</h2>
                     <a href=""><i className="fab fa-facebook"></i> Facebook</a>
@@ -27,7 +29,7 @@ const Footer = () => {
                     <a href=""><i className="fab fa-linkedin"></i> Linkedin</a>
                     <a href=""><i className="fab fa-instagram"></i> Instagram</a>
                     <a href=""><i className="fab fa-envelope"></i> pantheontech@gmail.com</a>
-                    <section className="box__copyright">
+                    <section className={`${FooterStyle.boxcopyright}`}>
                         <hr/>
                         <p>
                             Todos los derechos reservados © 2023 <b>Pantheon Tech</b>
