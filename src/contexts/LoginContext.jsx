4bae0da4a41d3eebf61f19fc1661c39090cfc11b
objetive218 <davidsantiagotc@gmail.com>
2023-04-25 15:42:21 -0500

    login context

--- src/contexts/LoginContext.jsx
+++ src/contexts/LoginContext.jsx
@@ -1,3 +1,11 @@
 import React,{createContext,useState} from "react";
 
 const LoginContext =  createContext();
+const LoginProvider = function({children}){
+  const [usuario, setUsuario] = useState(null)
+  return <LoginContext.Provider value={{usuario, setUsuario}}>
+    {children}
+  </LoginContext.Provider>
+}
+export { LoginProvider}
+export default LoginContext
