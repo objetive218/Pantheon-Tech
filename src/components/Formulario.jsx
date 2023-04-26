d608729c06d15bc3e4573d062b80061dba63d4c9
objetive218 <davidsantiagotc@gmail.com>
2023-04-26 11:14:00 -0500

    form corregido

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,44 +1,44 @@
-import React, {useState} from 'react';
+import React,{useState} from 'react';
 
 import FormularioStyle from "../css/Formulario.module.css"
 
-function Formulario (){
-    const [nombre, setNombre] = useState ("");
-    const [caracteristicas, setCaracteristicas] = useState("");
-    const [categoria, setCategoria] = useState("");
-    const [precio, setPrecio] = useState("");
-    const [imagen, setImagen] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log({nombre, caracteristicas, categoria, precio, imagen});
-    };
 
-    return(<>
-        <form onSubmit={handleSubmit} className='form'>
-            <label htmlFor='nombre'className='label'>
-                Nombre del artículo:</label>
-            <input type='text' id='nombre' name='nombre' value={nombre}
-            onChange={(event) => setNombre(event.target.value)}
-            required
-            className='input'
-            />
+const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [caracteristicas, setCaracteristicas] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [imagen, setImagen] = useState("");
 
-            <label htmlFor='caracteristicas' className='label'>
-                Características:</label>
-            <textarea
-            id='características'
-            name='características'
-            value={características}
-            onChange={(event) => setCaracteristicas(event.target.value)}
-            placeholder='Ingrese acá las siguientes características:
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log({nombre, caracteristicas, categoria, precio, imagen});
+  return (
+    <form onSubmit={handleSubmit} className='form'>
+      <label htmlFor='nombre'className='label'>
+      Nombre del artículo:</label>
+      <input type='text' id='nombre' name='nombre' value={nombre}
+      onChange={(event) => setNombre(event.target.value)}
+      required
+      className='input'
+      />
+      
+      <label htmlFor='caracteristicas' className='label'>
+      Características:</label>
+      <textarea
+      id='características'
+      name='características'
+      value={características}
+      onChange={(event) => setCaracteristicas(event.target.value)}
+      placeholder='Ingrese acá las siguientes características:
             Cámara: 50MP
             Memoria: 128GB
             Batería: 5000mAH'
             required
             className='input'
             />
-
+            
             <label htmlFor='categoria' className='label'>Categoría:</label>
             <select
             id='categoria'
@@ -48,14 +48,14 @@ function Formulario (){
             required
             className='input'
             >
-                <option value="">Selecciona una opción</option>
-                <option value="celulares">Celulares</option>
-                <option value="computadores">Computadores</option>
-                <option value="videojuegos">Videojuegos</option>
-                <option value="consolas">Consolas</option>
-                <option value="audio">Audio</option>
+            <option value="">Selecciona una opción</option>
+            <option value="celulares">Celulares</option>
+            <option value="computadores">Computadores</option>
+            <option value="videojuegos">Videojuegos</option>
+            <option value="consolas">Consolas</option>
+            <option value="audio">Audio</option>
             </select>
-
+            
             <label htmlFor='precio' className='label'>Precio COP:</label>
             <input
             type='number'
@@ -66,7 +66,7 @@ function Formulario (){
             required
             className='input'
             />
-
+            
             <label htmlFor='imagen' className='label'>URL de la imagen:</label>
             <input
             type='url'
@@ -77,10 +77,12 @@ function Formulario (){
             required
             className='input'
             />
-
+            
             <button type='submit' className='button'>Enviar</button>
-        <form/></>    
-    );
-}
+            </form>
+    
+  )
+}}
 
+            
 export default Formulario
