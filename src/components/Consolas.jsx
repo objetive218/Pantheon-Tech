a9433e8c9c89fb53c587f41284f4175f9c156534
“KarenL23” <lopezkaren23.05@gmail.com>
2023-04-26 15:36:13 -0500

    estructura consolas

--- src/components/Consolas.jsx
+++ src/components/Consolas.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import Modelo from "./Modelo";
+import NavBar from './NavBar';
+import CatalogoContext from '../contexts/CatalogoContext';
+import VideojuegoTitle from "../img/videojuegos.svg";
 
-const Consolas = () => {
+const Videojuegos = () => {
+  const { productos} = useContext(CatalogoContext);
+  const [video, setVideo] = useState([]); 
+  useEffect( 
+    function () {
+      setVideo(
+        productos.filter(function (item) {
+    return item.categoria == "videojuegos";
+    })
+  );
+},
+  [productos]
+  );
   return (
-    <div>Consolas</div>
+    <>
+    <NavBar />
+    <img src={VideojuegoTitle} alt="logo" />
+    <section>
+      {video != null
+        ? video.map(function (element, index) {
+            return (
+              <Modelo key = {index}
+                fuente={video[index]?.url}
+                modelo={video[index]?.modelo}
+                especificaciones={video[index]?.especificaciones}
+                precio={video[index]?.precio}
+                id={video[index]?.id}
+                elemento={video[index]}
+              />
+            );
+          })
+        : ""}
+    </section>
+    </>
   )
 }
 
-export default Consolas
+export default Videojuegos
