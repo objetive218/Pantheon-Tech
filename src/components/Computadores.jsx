48ce5c490810a1f6b4104b6861b1293b6caeec0a
“KarenL23” <lopezkaren23.05@gmail.com>
2023-04-26 15:12:30 -0500

    estructura

--- src/components/Computadores.jsx
+++ src/components/Computadores.jsx
@@ -1,16 +1,42 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Modelo from "./Modelo";
 import NavBar from './NavBar';
 import CatalogoContext from '../contexts/CatalogoContext';
+import ComputadorTitle from "../img/computadores.svg";
 
 const Computadores = () => {
   const { productos} = useContext(CatalogoContext);
-  const comp = catalogo.filter(function (item) {
-    return item.categoria == "celulares";
-  });
+  const [comp, setComp] = useState([]); 
+  useEffect( 
+    function () {
+      setComp(
+        productos.filter(function (item) {
+    return item.categoria == "computadores";
+    })
+  );
+},
+  [productos]
+  );
   return (
     <>
-    <NavBar/>
-    <div>Computadores</div>
+    <NavBar />
+    <img src={ComputadorTitle} alt="logo" />
+    <section>
+      {comp != null
+        ? comp.map(function (element, index) {
+            return (
+              <Modelo key = {index}
+                fuente={comp[index]?.url}
+                modelo={comp[index]?.modelo}
+                especificaciones={comp[index]?.especificaciones}
+                precio={comp[index]?.precio}
+                id={comp[index]?.id}
+                elemento={comp[index]}
+              />
+            );
+          })
+        : ""}
+    </section>
     </>
   )
 }
