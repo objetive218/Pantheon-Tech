7342220bacdcf834c464b7343f39656f7c7c7e29
objetive218 <davidsantiagotc@gmail.com>
2023-04-26 08:39:19 -0500

    contexto

--- src/contexts/LoginContext.jsx
+++ src/contexts/LoginContext.jsx
@@ -1,8 +1,13 @@
-import React,{createContext,useState} from "react";
+import React,{createContext,useState, useContext} from "react";
+import UserDataContext from "./UserDataContext";
 
 const LoginContext =  createContext();
 const LoginProvider = function({children}){
   const [usuario, setUsuario] = useState(null)
+  const {data} = useContext(UserDataContext);
+  const logeo = function(usuario, password){
+    const registrado = data.find()
+  }
   return <LoginContext.Provider value={{usuario, setUsuario}}>
     {children}
   </LoginContext.Provider>
