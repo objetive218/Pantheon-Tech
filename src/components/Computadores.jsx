0eb9268d430cbb3c136b8901f09ccbc2c2dd0ad1
“KarenL23” <lopezkaren23.05@gmail.com>
2023-04-26 11:46:37 -0500

    1 edicion comp

--- src/components/Computadores.jsx
+++ src/components/Computadores.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import NavBar from './NavBar';
+import CatalogoContext from '../contexts/CatalogoContext';
 
 const Computadores = () => {
+  const { productos} = useContext(CatalogoContext);
   const comp = catalogo.filter(function (item) {
     return item.categoria == "celulares";
   });
   return (
+    <>
+    <NavBar/>
     <div>Computadores</div>
+    </>
   )
 }
 
