a1d50849d4ff91ae1f2c1fef8c1dd4c219ce7d03
objetive218 <davidsantiagotc@gmail.com>
2023-04-26 22:05:21 -0500

    mas facil

--- src/data/UserData.js
+++ src/data/UserData.js
@@ -1,5 +1,5 @@
 const UserData = [];
 UserData.push({username: "karen23", password: "patoyganso" , roll: "administrador"})
-UserData.push({username: "carlos2023", password: "hola1234" , roll: "usuario"})
+UserData.push({username: "hola1234", password: "hola1234" , roll: "usuario"})
 
 export default UserData
