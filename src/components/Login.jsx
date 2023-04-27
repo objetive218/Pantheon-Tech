76ce420b71c470ce11b6f86980732ef301a97cd0
objetive218 <davidsantiagotc@gmail.com>
2023-04-27 09:09:07 -0500

    login test

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,43 +2,55 @@ import React, { useContext, useState } from "react";
 import LoginContext from "../contexts/LoginContext";
 import Formulario from "./Formulario";
 import { Link } from "react-router-dom";
+import LoginStyle from "../css/Login.module.css";
 
 const Login = () => {
   const { usuario, logeo, logout, esAdmin } = useContext(LoginContext);
   return (
     <>
-      {usuario ? (
-        <form
-          onSubmit={function (e) {
-            e.preventDefault();
-            logout();
-          }}
-        >
-          <span>
-            {usuario.username} - {usuario.roll}
-          </span>
-          <button>salir</button>
-        </form>
-      ) : (
-        <form
-          onSubmit={function (e) {
-            e.preventDefault();
-            const { elements } = e.target;
-            const { username, password } = elements;
-            logeo(username.value, password.value);
-          }}
-        >
-          <input type="text" placeholder="nombre" name="username" id="username" />
-          <input
-            type="password"
-            name="password"
-            placeholder="contraseña"
-            id="password"
-          />
-          <button>Ingresar</button>
-        </form>
-      )}
-      {esAdmin() ? <Link to="/formulario">Agregar un nuevo producto</Link> : "" }
+      <section className={LoginStyle.box}>
+        {usuario ? (
+          <form
+            onSubmit={function (e) {
+              e.preventDefault();
+              logout();
+            }}
+          >
+            <span>
+              {usuario.username} - {usuario.roll}
+            </span>
+            <button>salir</button>
+          </form>
+        ) : (
+          <form
+            onSubmit={function (e) {
+              e.preventDefault();
+              const { elements } = e.target;
+              const { username, password } = elements;
+              logeo(username.value, password.value);
+            }}
+          >
+            <input
+              type="text"
+              placeholder="nombre"
+              name="username"
+              id="username"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="contraseña"
+              id="password"
+            />
+            <button>Ingresar</button>
+          </form>
+        )}
+          </section>
+        {esAdmin() ? (
+          <Link to="/formulario">Agregar un nuevo producto</Link>
+        ) : (
+          ""
+        )}
     </>
   );
 };
