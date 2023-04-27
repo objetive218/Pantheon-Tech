acf4e56d005f0fff1d0beba4fc5a484b89e171f7
objetive218 <davidsantiagotc@gmail.com>
2023-04-27 09:43:48 -0500

    catalogo funcion

--- src/contexts/CatalogoContext.jsx
+++ src/contexts/CatalogoContext.jsx
@@ -4,8 +4,11 @@ import { catalogo } from "../data/catalogo";
 const CatalogoContext = createContext(catalogo);
 const CatalogoProvider = function({children}){
   const [productos, setProductos] = useState(catalogo);
+  const agregaItem = function(elemento, id){
+    
+  }
   
-  return <CatalogoContext.Provider value={{productos, setProductos}}>{children}</CatalogoContext.Provider>
+  return <CatalogoContext.Provider value={{productos, setProductos, agregaItem}}>{children}</CatalogoContext.Provider>
 }
 
 export { CatalogoProvider }
