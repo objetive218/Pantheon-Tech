ba01d24fcfec921c46e145b6ec34984dcdacba9b
“KarenL23” <lopezkaren23.05@gmail.com>
2023-05-01 18:34:11 -0500

    edicion footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,35 @@
 import FooterStyle from "../css/Footer.module.css";
 import Logo from "../img/logo.svg";
 import { Link } from "react-router-dom";
+import {FaEnvelope} from 'react-icons/fa'
 
 
 import React, { useState } from "react";
 
 const Footer = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  return (
-    <section className={`${FooterStyle.containerFooter}`}>
-      <section className={`${FooterStyle.boxfooter}`}>
-        <picture className={`${FooterStyle.logo}`}>
+  return(
+      <footer id={`${FooterStyle.footerMain}`}>
+        <picture id={`${FooterStyle.footerLogo}`}>
           <img src={Logo} alt="logo" /> </picture>
-          <section className={`${FooterStyle.terms}`}>
-            <section className={`${FooterStyle.boxfooter}`}>
-            <Link to="/nosotros">
-                  ¿Quiénes somos?
-                </Link>
-              <h2>Términos y Condiciones</h2>
-              {!isClicked && (
-                <button onClick={() => setIsClicked(!isClicked)}>
-                  Ver más
-                </button>
-              )}
-              {isClicked && (
-                <Link to="/terminos">
-                  Términos y condiciones
-                </Link>
-              )}
-            </section>
-            <h2>Contacto</h2>
-            <a href="">
-              <i className="fab fa-envelope"></i> pantheontech@gmail.com
+            <section id={`${FooterStyle.copyright}`}>
+            <h3>Contacto</h3>
+            <a href="mailto:pantheontech@gmail.com" target="_blank">
+            <FaEnvelope/>
+            pantheontech@gmail.com
             </a>
-            <section className={`${FooterStyle.boxcopyright}`}>
-              <hr />
               <p>
                 Todos los derechos reservados © 2023 <b>Pantheon Tech</b>
               </p>
             </section>
-          </section>
-        </section>
-      </section>
+            <nav id={`${FooterStyle.footerLinks}`}>
+            <Link to="/nosotros">
+            ¿Quiénes somos?
+            </Link>
+            <Link to="/terminos">
+            Términos y condiciones
+            </Link>
+            </nav>
+        </footer>
   );
 };
 
