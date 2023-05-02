3711bf1deb755ed624470b0d16f231a011fc335a
“KarenL23” <lopezkaren23.05@gmail.com>
2023-05-02 15:07:41 -0500

    links

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -8,6 +8,7 @@ import testcel from "../img/celpreviewtest.png";
 import testcomp from "../img/comppreview.png";
 import testvid from "../img/vidpreview.png";
 import testaudio from "../img/audiopreview.png";
+import { Link } from "react-router-dom";
 
 const Preview = () => {
   const [cambio, setCambio] = useState("");
@@ -28,11 +29,13 @@ const Preview = () => {
             src={categoriacelular}
             alt="telefono"
           />
+            <Link to="/celulares" >
           <img
             className={cambio == "celular" ? StylePreview.preview : StylePreview.active}
             src={testcel}
             alt="telefono"
-          />
+            />
+            </Link>
         </picture>
         <picture
           onMouseEnter={function () {
@@ -48,11 +51,13 @@ const Preview = () => {
             src={categoriacomputador}
             alt="computador"
           />
+            <Link to="/computadores" >
           <img
             className={cambio == "computador" ? StylePreview.preview : StylePreview.active}
             src={testcomp}
             alt="computador"
-          />
+            />
+            </Link>
         </picture>
         <picture
           onMouseEnter={function () {
@@ -68,11 +73,13 @@ const Preview = () => {
             src={categoriaconsola}
             alt="consola"
           />
+            <Link to="/consolas" >
           <img
             className={cambio == "videojuego" ? StylePreview.preview : StylePreview.active}
             src={testvid}
             alt="consola"
-          />
+            />
+            </Link>
         </picture>
         <picture
           onMouseEnter={function () {
@@ -88,11 +95,13 @@ const Preview = () => {
             src={categoriaaudio}
             alt="audio"
           />
+            <Link to="/audio" >
           <img
             className={cambio == "audio" ? StylePreview.preview : StylePreview.active}
             src={testaudio}
             alt="audio"
           />
+        </Link>
         </picture>
       </section>
     </>
