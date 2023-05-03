624262b4ec6bd498190798665e2d982a2cec9b39
objetive218 <davidsantiagotc@gmail.com>
2023-05-03 14:01:36 -0500

    buscador

--- src/components/Computadores.jsx
+++ src/components/Computadores.jsx
@@ -2,12 +2,27 @@ import React, { useContext, useState, useEffect } from 'react';
 import Modelo from "./Modelo";
 import NavBar from './NavBar';
 import CatalogoContext from '../contexts/CatalogoContext';
+import BuscadorContext from "../contexts/BuscadorContext";
 import ComputadorTitle from "../img/computadores.svg";
 import StyleComputadores from "../css/Computadores.module.css"
 
 const Computadores = () => {
   const { productos} = useContext(CatalogoContext);
   const [comp, setComp] = useState([]); 
+  const { buscar } = useContext(BuscadorContext);
+  const [carga, setCarga] = useState([]);
+  useEffect(
+    function () {
+      setCarga(
+        productos.filter(function (item) {
+          return String(item.modelo)
+            .toLowerCase()
+            .includes(String(buscar).toLowerCase());
+        })
+      );
+    },
+    [buscar]
+  );
   useEffect( 
     function () {
       setComp(
@@ -21,7 +36,9 @@ const Computadores = () => {
   return (
     <>
     <NavBar />
-    <picture className={StyleComputadores.comput}>
+    {buscar == null || String(buscar).length > 3 ? null : (
+          <>
+          <picture className={StyleComputadores.comput}>
     <img src={ComputadorTitle} alt="logo" />
     </picture>
     <section className={StyleComputadores.articulos} >
@@ -40,6 +57,25 @@ const Computadores = () => {
           })
         : ""}
     </section>
+          </>
+        )}
+    {buscar == null || String(buscar).length > 3 ? (
+          carga.map(function (element, index) {
+            return (
+              <Modelo
+                key={index}
+                fuente={carga[index]?.url}
+                modelo={carga[index]?.modelo}
+                especificaciones={carga[index]?.especificaciones}
+                precio={carga[index]?.precio}
+                id={carga[index]?.id}
+                elemento={carga[index]}
+              />
+            );
+          })
+        ) : (
+          ""
+        )}
     </>
   )
 }
