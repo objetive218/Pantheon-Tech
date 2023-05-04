364f2e8bc3f3299ad4fb7b757acae40175c4ade8
objetive218 <davidsantiagotc@gmail.com>
2023-05-03 20:27:47 -0500

    añadir eliminar

--- src/components/Modelo.jsx
+++ src/components/Modelo.jsx
@@ -1,39 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useState , useEffect} from "react";
 import modeloStyle from "../css/Modelo.module.css";
 import CarritoContext from "../contexts/CarritoContext";
-import Borde from "../img/borde.svg"
+import Borde from "../img/borde.svg";
 import { BsCart2 } from "react-icons/bs";
 import numeral from "numeral";
+import LoginContext from "../contexts/LoginContext";
+import CatalogoContext from "../contexts/CatalogoContext";
 
 const Modelo = (props) => {
   const { eliminarModel, addModel } = useContext(CarritoContext);
+  const { borrar } = useContext(CatalogoContext);
+  const { esAdmin } = useContext(LoginContext);
+  const [provador, setProvador] = useState(null)
+  useEffect(function(){
+    setProvador(esAdmin)
+  },[esAdmin])
   return (
     <>
       <section className={modeloStyle.modelo}>
-        <img className={modeloStyle.bordecito} src={Borde} alt="BordeArticulo"/>
-        <section id={modeloStyle.imgtl}>
-
         <img
-          className={modeloStyle.image}
-          src={props.fuente}
-          alt="imagen modelo"
+          className={modeloStyle.bordecito}
+          src={Borde}
+          alt="BordeArticulo"
+        />
+        <section id={modeloStyle.imgtl}>
+          <img
+            className={modeloStyle.image}
+            src={props.fuente}
+            alt="imagen modelo"
           />
-        <h2 className={modeloStyle.nombre} >{props.modelo}</h2>
-          </section>
+          <h2 className={modeloStyle.nombre}>{props.modelo}</h2>
+        </section>
         <p className={modeloStyle.caracteristica}>{props.especificaciones}</p>
-        <section id={modeloStyle.cajita} >
-        <p className={modeloStyle.valor}>{`$ ${numeral(props.precio).format('0,0')}`}</p>
-        <form action="#">
-          <button className={modeloStyle.carrito}
+        <section id={modeloStyle.cajita}>
+          <p className={modeloStyle.valor}>{`$ ${numeral(props.precio).format(
+            "0,0"
+          )}`}</p>
+          <form action="#">
+            <button
+              className={modeloStyle.carrito}
+              type="button"
+              onClick={function () {
+                addModel(props.elemento, 1);
+              }}
+            >
+              <BsCart2 />
+            </button>
+          </form>
+        </section>
+        <form className={provador ? "": modeloStyle.active }>
+          <button
             type="button"
             onClick={function () {
-              addModel(props.elemento, 1);
+              borrar(props.id);
             }}
           >
-          <BsCart2 />
+            -
           </button>
         </form>
-        </section>
       </section>
     </>
   );
